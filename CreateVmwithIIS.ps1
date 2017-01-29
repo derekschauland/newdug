5e316f970e77f40d00545c7507b5555c9f4371da
Derek Schauland <derek@derekschauland.com>
2017-01-29 16:18:34 -0600

    new dsc

--- CreateVmwithIIS.ps1
+++ CreateVmwithIIS.ps1
@@ -1,62 +1,11 @@
 ﻿configuration CreateVMwithIIS
-{ 
-   param 
-   ( 
-        [Parameter(Mandatory)]
-        [String]$computername = 'localhost'
-
-       
-    ) 
-    
-    Import-DscResource -ModuleName PSDesiredStateConfiguration
- 
-
-    [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("$DomainName\$($AdminCreds.UserName)", $AdminCreds.Password)
-
-    Node $AllNodes.NodeName
-    {
-        LocalConfigurationManager
+{
+         node "localhost"
         {
-            ActionAfterReboot = 'ContinueConfiguration'
-            ConfigurationMode = 'ApplyOnly'
-            RebootNodeIfNeeded = $true
-            AllowModuleOverWrite = $true
+            WindowsFeature IIS
+            {
+                Ensure = "Present"
+                Name = "Web-Server"
+            }
         }
-
-            WindowsFeature Management {
- 
-            Name = 'Web-Mgmt-Service'
-            Ensure = 'Present'
-            DependsOn = @('[WindowsFeature]IIS')
-        }
- 
-        Registry RemoteManagement {
-            Key = 'HKLM:\SOFTWARE\Microsoft\WebManagement\Server'
-            ValueName = 'EnableRemoteManagement'
-            ValueType = 'Dword'
-            ValueData = '1'
-            DependsOn = @('[WindowsFeature]IIS','[WindowsFeature]Management')
-       }
- 
-       Service StartWMSVC {
-            Name = 'WMSVC'
-            StartupType = 'Automatic'
-            State = 'Running'
-            DependsOn = '[Registry]RemoteManagement'
- 
-       }
-
-        WindowsFeature IIS 
-        { 
-            Ensure = 'Present'
-            Name = 'Web-Server'
-        }  
-
-        WindowsFeature ASP
-        { 
-            Ensure = 'Present' 
-            Name = 'Web-Asp-Net45' 
-        }
-
-   }
 }
